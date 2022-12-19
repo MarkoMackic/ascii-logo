eeaec5250a873ccb5f94028fc8d8c2864063d131
Marko Mackic <markomackic@gmail.com>
2022-12-19 03:17:45 +0100

    Let's go, let's go

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 ## Nothing special, nothing much.
 
 Basic ASCII logo generator!
+
+Required Python version : `>=3.7`
+
+## Usage
+
+```
+usage: generator.py [-h] [-t TEXT] [-s SIZE] [-f FONT] [-fs FONT_SIZE] [-c COLOR] [-bg BACKGROUND_COLOR] [-o OUTPUT]
+
+options:
+  -h, --help            show this help message and exit
+  -t TEXT, --text TEXT  Logo text
+  -s SIZE, --size SIZE  Image size in format x,y
+  -f FONT, --font FONT  Font file rendering ASCII art
+  -fs FONT_SIZE, --font-size FONT_SIZE
+                        Font size for rendering final image
+  -c COLOR, --color COLOR
+                        Foreground color
+  -bg BACKGROUND_COLOR, --background-color BACKGROUND_COLOR
+                        Background color
+  -o OUTPUT, --output OUTPUT
+                        Output file path
+```
+
+If output is specifed then it will save the image, otherwise it'll just show you the image.
+Background is transparent by default.
+Fonts are TTF only.
+Experiment with font size param, and ASCII_GEN_FONT_SIZE environent to tweak the sizes.
+
+
+## Example
+
+![Example image](example.png)
---
+++ generator.py
@@ -0,0 +1,80 @@
+import argparse, os
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+
+def generate_ascii_art(text, font):
+    font_variant = font.font_variant(size=int(os.environ.get('ASCII_GEN_FONT_SIZE', '50')))
+    mask = font_variant.getmask(text, mode="1")
+    width,height = mask.size
+
+    lines = []
+
+    for y in range(height):
+        single_char_width = width // len(text)
+        line = width * ['']
+        for x in range(width):
+            line[x] = text[x // single_char_width] if mask.getpixel((x,y)) > 0 else ' '
+
+        lines.append(''.join(line))
+
+    return lines
+
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-t', '--text', help='Logo text')
+    arg_parser.add_argument('-s', '--size', help='Image size in format x,y')
+    arg_parser.add_argument('-f', '--font', help='Font file rendering ASCII art')
+    arg_parser.add_argument('-fs', '--font-size', help="Font size for rendering final image")
+    arg_parser.add_argument('-c', '--color', help='Foreground color')
+    arg_parser.add_argument('-bg', '--background-color', help='Background color')
+    arg_parser.add_argument('-o', '--output', help='Output file path')
+
+    args = arg_parser.parse_args()
+
+    assert not args.text is None, 'Text parameter must be supplied'
+
+    im_size = tuple([int(size) for size in args.size.split(',')]) if not args.size is None else (200, 200)
+
+    print('Image size is : ', im_size)
+
+    im = Image.new(mode = 'RGBA', size = im_size, color=args.background_color)
+
+    im_font = ImageFont.truetype(font = args.font or "./fonts/SourceCodePro-ExtraBold.ttf", size=int(args.font_size or 8));
+
+    generated_art_str = '\n'.join(generate_ascii_art(args.text, im_font))
+
+    im_draw = ImageDraw.Draw(im)
+
+    (o_left, o_top, o_right, o_bottom) =  im_draw.textbbox(xy=tuple(map(lambda i: i // 2, im_size)), text=generated_art_str, anchor="mm")
+
+    if o_left < 0 or o_top < 0 or o_right > im_size[0] or o_bottom > im_size[1]:
+        print("Minimal image size for this configuration is", o_right - o_left, ',',  o_bottom - o_top)
+        exit(1)
+
+    # try to adjust font size based on space available
+
+    font = im_font
+
+    while True:
+        new_font = font.font_variant(size=font.size + 1)
+
+        (o_left, o_top, o_right, o_bottom) =  im_draw.textbbox(xy=tuple(map(lambda i: i // 2, im_size)), text=generated_art_str, anchor="mm", font=new_font)
+
+        if o_left < 0 or o_top < 0 or o_right > im_size[0] or o_bottom > im_size[1]:
+            break;
+
+        font = new_font
+
+
+    im_draw.font = font
+
+    im_draw.text(xy=tuple(map(lambda i: i // 2, im_size)), text=generated_art_str, fill=ImageColor.getrgb(args.color or 'black'), anchor="mm")
+
+    if not args.output is None:
+        im.save(args.output)
+    else:
+        im.show()
+
+if __name__ == '__main__':
+    main()
